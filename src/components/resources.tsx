4eca88e65a1b6e90419672aa2ac497d794cda3c4
halfordjakson <halfordjakson@gmail.com>
2025-09-13 01:00:51 -0500

    fix: unify Resources page banner with full-width highlight-bar

--- src/components/resources.tsx
+++ src/components/resources.tsx
@@ -2,23 +2,19 @@ import React from "react";
 import Navigation from "./navigation";
 import Footer from "./footer";
 import "../styles/resources.css";
+import "../styles/about.css";
 export default function Resources() {
 
     return (
         <React.Fragment>
             <Navigation />
+            <div className="highlight-bar">Resources</div>
             <div className="rsr-r">
                 <div className="rsr-dec"></div>
-                <div className="rsr-t">
-                    <h2>Resources</h2>
-                </div>
                 <div className="rsr-cc">
                     <div className="rsr-c">
-
                     </div>
                 </div>
-
-
             </div>
             <Footer />
         </React.Fragment>
